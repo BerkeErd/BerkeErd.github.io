5eb2d820ba67846147a15d42f868d24dfb0891f0
BerkeErd <39529687+BerkeErd@users.noreply.github.com>
2023-09-05 21:35:47 +0300

    Updated.

--- js/main.js
+++ js/main.js
@@ -1,34 +1,211 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const projectDetails = {
-      "stick-slasher": "Stick Slasher oyununun detayları burada olacak.",
-      // Diğer projelerin detayları
-    };
+const projectDetails = {
+  "stick-slasher": {
+    name: "Stick Slasher",
+    image: "https://cdn.cloudflare.steamstatic.com/steam/apps/2549950/header.jpg?t=1693580140",
+    description: "Stick Slasher is a fast-paced, action-packed 2D fighting game where players wield a variety of weapons to fend off endless waves of enemies. As you vanquish foes, increasingly diverse and challenging adversaries emerge. Whether you decapitate them, sever their limbs, or strike elsewhere, each kill brings its own satisfaction.",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.BerukeGames.StickSlasher",
+    steam: "https://store.steampowered.com/app/2549950/Stick_Slasher/",
+    itch: "https://beruke.itch.io/sslasher",
+    webGL: "#",
+    youtube: "https://www.youtube.com/watch?v=MSdYlAcypdM",
+    additionalImages: ["https://cdn.cloudflare.steamstatic.com/steam/apps/2549950/ss_a11d221e03480e3dc92c0ad651c20c05af3c1600.600x338.jpg?t=1693580140", "https://cdn.cloudflare.steamstatic.com/steam/apps/2549950/ss_466284956c1f1507911b6d83b427114172437fbc.600x338.jpg?t=1693580140"],
+    color: "#F1F1F1",
+    backgroundColor: "#2a2a2a"
+  },
+  "sesli-sorulu-milyonlar": {
+    name: "Sesli Sorulu Milyonlar",
+    image: "https://play-lh.googleusercontent.com/aH_-37UFLVfxqSQXQlKS3Gug2HVY_mKaJ5B72JivVsdvSgCPLXlL76SYZTA7GR6hwZjT=w416-h235-rw",
+    description: "Stick Slasher is a fast-paced, action-packed 2D fighting game where players wield a variety of weapons to fend off endless waves of enemies. As you vanquish foes, increasingly diverse and challenging adversaries emerge. Whether you decapitate them, sever their limbs, or strike elsewhere, each kill brings its own satisfaction.",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.Beruke.SesliSoruMilyoner2021",
+    steam: "#",
+    itch: "#",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/emxtrseac2hPQv4wlS0wtbleLRea5rF1aqg4Y8P-UhJ4OMdg8E99_QkO-6uZDgajw-0=w2560-h1440-rw", "https://play-lh.googleusercontent.com/Zy0kebYkz72gwV6zMgrlOOd1hUeWQp4-rU_VjBE4Rx07R9CjjXRByhlK2ivNSzPOJw=w2560-h1440-rw"],
+    color: "#F1F1F1",
+    backgroundColor: "#6000bf"
+  },
+  "ability-runner": {
+    name: "Ability Runner - Evolve",
+    image: "https://play-lh.googleusercontent.com/6A-AFPByB7Ik_X_R9_P-Om81fBn5jHf9L4pOgOryq_xqTCxmSbHDubuouEIz9xfSiwQ=w416-h235-rw",
+    description: "Unleash abilities in this endless adventure. Can you conquer the race?",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.Beruke.AbilityRunner",
+    steam: "#",
+    itch: "https://beruke.itch.io/ability-runner",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/AsrrB4QX8cme5A6f_372xy1eIBXN75kBB6durgWL3dAQUgUSkZIN9tmy31RTmI6gO5A=w2560-h1440-rw", "https://play-lh.googleusercontent.com/kCi8SXO0jakfeZvqWBMpCjjdEXs2_c0NGu-kfyJnv8CUaHGAKT_fBFhhTdc1CBj9eJc=w2560-h1440-rw"],
+    color: "#000000",
+    backgroundColor: "#ddf8ff"
+  },
+  "patlat-bi-kelime": {
+    name: "Patlat Bi Kelime",
+    image: "https://play-lh.googleusercontent.com/2gU4ecNsL31uBwAMGGdgJ89uBQSU8_N-PKqxfgwXxHlIRHvZye8PQAki-Lp7JL18CcU=w240-h480-rw",
+    description: "Bomba patlamadan kelimeyi bul!",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.BerukeGames.PatlatBiKelime",
+    steam: "#",
+    itch: "#",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/93OOOSOGiqF123bhBgSBCHFUI897XxfRlyGehJGOKhm7mkHgYfcH9cA4jTCbfmDfzqBO=w526-h296-rw", "https://play-lh.googleusercontent.com/apNCK561KuakiNvY2BUBCTfs8ZMLFWenjjnbA4Y8512lg__e1ylB-wKASAgB8f43_Q=w526-h296-rw"],
+    color: "#F1F1F1",
+    backgroundColor: "#0014bd"
+  },
+  "bubble-pass": {
+    name: "Bubble Pass",
+    image: "https://play-lh.googleusercontent.com/I1aPImKuxw9oku1aRV5Tug06Fm9kSw1wFS29uZnG5NtE78fJ6sqdK4AvEcolG1h32oQ=w416-h235-rw",
+    description: "Match filter to the bubbles color to survive in this fast and colorful game.",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.BerukeGames.BubblePass",
+    steam: "#",
+    itch: "#",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/6tS-hW7NLv3O2mkIKkJJ9PlG8SpzENXy-ACCbaV2XkvEzKZCS3QpJruKDyQQWNvODMM=w526-h296-rw", "https://play-lh.googleusercontent.com/d6DZ_SXqMzrMnIIKVfpgb0YVf9DWt8950_yJs-GCpQqAoZy4XHsRfJdVk3pdpzgxxGE=w526-h296-rw"],
+    color: "#F1F1F1",
+    backgroundColor: "#1ab5f2"
+  },
+  "simple-kalimba": {
+    name: "Simple Kalimba",
+    image: "https://play-lh.googleusercontent.com/fNIUcRC5siSf1VH5CSRmJLmhCC3fzbSXNqa_ePOsQieCotMTa66llKgDHalKS3UEqZg=w416-h235-rw",
+    description: "Just a simple app for playing kalimba",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.Beruke.SimpleKalimba",
+    steam: "#",
+    itch: "#",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/m6uyZFaL8DQ1D84Ef_uOHdjS-rcZblI8EmYN1jcZiDCAu63nOYxkoZwjJ2lCWcvW0joo=w2560-h1440-rw", "https://play-lh.googleusercontent.com/woU8BqnyI-Pg2YtnhXTUAqqbmgCJ5nesVATWqO9NYs3gEQuh-3fE2G4jucPmygC1qnRV=w2560-h1440-rw"],
+    color: "#F1F1F1",
+    backgroundColor: "#8e2b08"
+  },
+  "bilgi-kat": {
+    name: "Bilgi Kat",
+    image: "https://play-lh.googleusercontent.com/iCqe_1nUIeHO1x0qx0nc929_qAXL_PqH-H1-vPv2TKxNWhPOtaxeNH2YsnlO8Uo1IyE=w416-h235-rw",
+    description: "Bilgi Kat ile 45 kategoride bilginizi ölçün, kat kat zorluklarla rekor kırın!",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.BerukeGames.BilgiKat",
+    steam: "#",
+    itch: "#",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/XfDDe6QKd5IApgkCRWWaZ5ohWCoqQQdqlnmfRx1EBCTnH8RON0RtmdS4JW5fFkFzEJo=w2560-h1440-rw", "https://play-lh.googleusercontent.com/MmiH3tDQmbfOZd3RxuTB-elXtpzY9Sz5I16JQG7Rjr9FY3RVfmj1MMSgU7AUbYtXIQ=w2560-h1440-rw"],
+    color: "#F1F1F1",
+    backgroundColor: "#f7ac1d"
+  },
+  "timeloop-traffic": {
+    name: "TimeLoop Traffic",
+    image: "https://play-lh.googleusercontent.com/BjNVTvEkKtDVvRgOhcy0MltQQTQj-A7aYpBT315xHNxvdXEGUs1ImmBKLJ4uBW9dlRM=w416-h235-rw",
+    description: "Drive, clone, control! Create your own traffic chaos in a time-looping city!",
+    googlePlay: "https://play.google.com/store/apps/details?id=com.BerukeGames.TimeLoopTrafficAutoCloneCity",
+    steam: "#",
+    itch: "https://beruke.itch.io/timeloop-traffic-autoclone-city",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/fFkzusy5Llnkz8QcNGtDm_LPH1yKsJKckBPc2g66syNkcCN2RZaQa7enHVgoQC_lL-bz=w2560-h1440-rw", "https://play-lh.googleusercontent.com/p0E4399rmAHCJ5N3aliEdprUrTRLjFTJ657EIXjwUUTyrqyX4pm3a_V_6l8M1IxGszo4=w2560-h1440-rw"],
+    color: "#000000",
+    backgroundColor: "#F1F1F1"
+  },
+  "rgb-square": {
+    name: "RGB Square",
+    image: "https://img.itch.zone/aW1nLzEyNTE0NTgwLnBuZw==/315x250%23c/Z1jLbC.png",
+    description: "Collect the color you needed to survive!",
+    googlePlay: "#",
+    steam: "#",
+    itch: "https://beruke.itch.io/rgb-square",
+    webGL: "#",
+    youtube: "#",
+    additionalImages: ["https://play-lh.googleusercontent.com/emxtrseac2hPQv4wlS0wtbleLRea5rF1aqg4Y8P-UhJ4OMdg8E99_QkO-6uZDgajw-0=w2560-h1440-rw", "https://play-lh.googleusercontent.com/Zy0kebYkz72gwV6zMgrlOOd1hUeWQp4-rU_VjBE4Rx07R9CjjXRByhlK2ivNSzPOJw=w2560-h1440-rw"],
+    color: "#000000",
+    backgroundColor: "#FFFFFF"
+  }
+  // diğer projeler
+};
+
+let lastColor = "white"; // Varsayılan olarak beyaz
+let lastBackgroundColor = "white"; // Varsayılan olarak siyah
+
+$(document).ready(function() {
+  $(".project-card").click(function() {
+    
+    $(".project-card").css("transform", "scale(1)");
+    $(this).css("transform", "scale(1.1)");
+
+    const projectKey = $(this).data("project");
+    const project = projectDetails[projectKey];
+
+
   
-    const projectDetailsDiv = document.getElementById('project-details');
-  
-    $('#projectCarousel').on('slid.bs.carousel', function () {
-      const activeItem = document.querySelector('.carousel-item.active');
-      const projectId = activeItem.getAttribute('data-project');
-      projectDetailsDiv.innerHTML = projectDetails[projectId];
-    });
+    let buttonsHtml = '';
+    if (project.googlePlay !== '#') buttonsHtml += `<a href="${project.googlePlay}" class="btn btn-primary">Google Play</a>`;
+    if (project.steam !== '#') buttonsHtml += `<a href="${project.steam}" class="btn btn-secondary">Steam</a>`;
+    if (project.itch !== '#') buttonsHtml += `<a href="${project.itch}" class="btn btn-info">Itch.io</a>`;
+    if (project.webGL !== '#') buttonsHtml += `<a href="${project.webGL}" class="btn btn-success">Oyna</a>`;
+    if (project.youtube !== '#') buttonsHtml += `<a href="${project.youtube}" class="btn btn-danger">YouTube</a>`;
+
+    const projectDetailsHtml = `
+      <div class="card">
+        <div class="card-header">
+          <h3>${project.name}</h3>
+        </div>
+        <div class="card-body">
+          <div class="row">
+            <div class="col-md-6">
+              <img src="${project.image}" alt="${project.name}" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+              <p>${project.description}</p>
+              <div class="btn-group" role="group" aria-label="Project Links">
+                ${buttonsHtml}
+              </div>
+            </div>
+          </div>
+          <div class="row mt-4">
+            <div class="col-md-6">
+              <img src="${project.additionalImages[0]}" alt="Additional Image 1" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+              <img src="${project.additionalImages[1]}" alt="Additional Image 2" class="img-fluid">
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    
+
+    $("#project-details").html(projectDetailsHtml);
+
+     
+   
+
+  // CSS'i güncelle
+  $("#project-details .card-body").css({
+    "color": lastColor,
+    "background-color": lastBackgroundColor,
+    "transition": "background-color 0.5s ease, color 0.5s ease"
   });
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const projectDetails = {
-      "stick-slasher": "Stick Slasher oyununun detayları burada olacak.",
-      // Diğer projeler
-    };
-  
-    const projectModal = document.getElementById('projectModal').querySelector('.modal-body');
-  
-    document.querySelectorAll('.project-card').forEach(function(card) {
-      card.addEventListener('click', function() {
-        const projectId = card.getAttribute('data-project');
-        projectModal.innerHTML = projectDetails[projectId];
-        $('#projectModal').modal('show');
-      });
+    // Önce transition özelliğini ayarla
+$("#project-details .card-body").css({
+  "transition": "background-color 0.5s ease, color 0.5s ease"
+});
+
+
+
+  // Biraz bekleyip sonra renkleri değiştir
+  setTimeout(function() {
+    $("#project-details .card-body").css({
+      "color": project.color,
+      "background-color": project.backgroundColor
     });
+  }, 50); 
+    
+    // Son renkleri güncelle
+    lastColor = project.color;
+    lastBackgroundColor = project.backgroundColor;
   });
+
+  
+});
+
   
   
   
